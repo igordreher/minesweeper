1f2ca3fce48f5fc8c093a501343939fd7f87f53c
igordreher <igor.dreher@gmail.com>
2022-03-12 12:05:21 -0300

    Center grid

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -13,18 +13,23 @@ fn create_board(mut commands: Commands) {
     let tile_map = tile::TileMap::new(10, 10, 5);
 
     let tile_size = 50.0;
-    let offset = 5.0;
+    let offset = Vec3::new(
+        ((tile_map.width - 1) as f32) * tile_size / 2.,
+        ((tile_map.height - 1) as f32) * tile_size / 2.,
+        1.,
+    );
     let mut sprites = vec![];
 
     for y in 0..tile_map.height {
         for x in 0..tile_map.width {
+            let pos = Vec3::new((x as f32) * tile_size, (y as f32) * tile_size, 1.);
             sprites.push(SpriteBundle {
                 sprite: Sprite {
                     color: Color::GRAY,
-                    custom_size: Some(Vec2::new(tile_size - offset, tile_size - offset)),
+                    custom_size: Some(Vec2::new(tile_size - 1., tile_size - 1.)),
                     ..Default::default()
                 },
-                transform: Transform::from_xyz((x as f32) * tile_size, (y as f32) * tile_size, 1.),
+                transform: Transform::from_translation(pos - offset),
                 ..Default::default()
             })
         }
